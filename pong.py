27e5f394ed275d08aa2918c193a2c9873787fa84
Arkadiusz Biel <arkadiusz.biel@nexttry.pl>
2017-10-09 12:26:29 +0100

    datebase connecting and file

--- pong.py
+++ pong.py
@@ -1,4 +1,15 @@
 from tinydb import Query
 from tinydb import TinyDB
 import pygame, sys
-from pygame.locals import *
+#db config
+db = TinyDB('user.json')
+users_table = db.table('users')
+scores = db.table('scores')
+def addscore():
+    global users_table, db, scores
+    # lastid
+    user_count = len(scores)
+    scores.insert({'score': int(v2.get()), 'uid': int(v.get()), 'id': user_count+1})
+
+def seescore():
+    global users_table, db, scores
