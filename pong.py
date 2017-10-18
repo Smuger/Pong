59766d627ceb1e9140bd7b369537e304196b88c7
Smuger <krzysiek.kwietniewski@gmail.com>
2017-10-18 17:38:15 +0100

    F: MainLoop; ball_init(), draw()

--- pong.py
+++ pong.py
@@ -6,11 +6,24 @@ from pygame.locals import *
 
 pygame.init()
 
+#Zmienne
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+HEIGHT = 1600
+WIDTH = 900
+window = pygame.display.set_mode((WIDTH,HEIGHT),0,32)
+
 def ball_init():
 
-def init():
 
-def draw():
+def init():
+    global paddle1_pos, paddle2_pos
+
+def draw(canvas):
+    global paddle1_pos, paddle2_pos, paddle1_vel, paddle2_vel, ball_pos
+    global l_score, r_score
+
+    pygame.canvas.fill(WHITE,0,0)
 
 def keydown(event):
     global paddle1_vel, paddle2_vel
