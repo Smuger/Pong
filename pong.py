b47833d786b210bfce996149f9e78aaa10edee52
Smuger <krzysiek.kwietniewski@gmail.com>
2017-10-23 19:10:03 +0100

    Nie wiem jak zrobic reszte wiec narysowalem na razie

--- pong.py
+++ pong.py
@@ -4,45 +4,38 @@ import random
 import pygame, sys
 from pygame.locals import *
 
-pygame.init()
-
-#Zmienne
-WHITE = (255, 255, 255)
+HEIGHT = 600
+WIDTH = 800
 BLACK = (0, 0, 0)
-HEIGHT = 1600
-WIDTH = 900
-window = pygame.display.set_mode((WIDTH,HEIGHT),0,32)
+WHITE = (255, 255, 255)
+H_WIDTH = int(WIDTH/2)
+H_HEIGHT = int(HEIGHT/2)
+score1 = 0
+score2 = 0
+pygame.init()
+pygame.font.init()
 
-def ball_init():
+window = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
+pygame.display.set_caption("siusiak")
+window.fill(WHITE)
+pygame.draw.rect(window, BLACK, (0, 8, WIDTH, HEIGHT - 16))
+pygame.draw.rect(window, WHITE, (32, H_HEIGHT - 32, 16, 64))
+pygame.draw.rect(window, WHITE, (WIDTH - 48, H_HEIGHT - 32, 16, 64))
+pygame.draw.circle(window, WHITE, (H_WIDTH, H_HEIGHT), 10, 0)
+pygame.draw.line(window, WHITE, (H_WIDTH, 0), (H_WIDTH, HEIGHT))
 
+text = pygame.font.SysFont("", 64)
+score = text.render( (str(score1) + " : " + str(score2)), 1, WHITE)
 
-def init():
-    global paddle1_pos, paddle2_pos
+score_width, score_height = text.size((str(score1) + " : " + str(score2)))
+window.blit(score, (H_WIDTH - score_width/2, 32))
 
-def draw(canvas):
-    global paddle1_pos, paddle2_pos, paddle1_vel, paddle2_vel, ball_pos
-    global l_score, r_score
+pygame.display.update()
+running = True
 
-    pygame.canvas.fill(WHITE,0,0)
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-def keydown(event):
-    global paddle1_vel, paddle2_vel
-
-    if event.key == K_UP:
-        paddle2_vel = -8
-    elif event.key == K_DOWN:
-        paddle2_vel = 80
-    elif event.key == K_w:
-        paddle1_vel = 8
-    elif event.key == K_s:
-        paddle1_vel = -8
-
-def keyup(event):
-
-    global paddle1_vel, paddle2_vel
-
-    if event.key (K_UP, K_DOWN):
-        paddle2_vel = 0
-    elif event.key (K_w, K_s):
-        paddle1_vel = 0
 
