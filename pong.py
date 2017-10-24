bf67b2bce1ea9cb82174d478d64282f334f99c8e
Smuger <krzysiek.kwietniewski@gmail.com>
2017-10-24 10:28:48 +0100

    kolizja paletki

--- pong.py
+++ pong.py
@@ -15,10 +15,12 @@ score2 = 0
 paddle1_vel = 0
 paddle2_vel = 0
 L_or_R = random.randint(0, 1)
-if L_or_R == 0:
-    ball_vel = [random.uniform(-1, -0.5), random.uniform(-1, 1)]
-else:
-    ball_vel = [random.uniform(0.5, 1), random.uniform(-1, 1)]
+# PLASKA PARABOLA
+ball_vel = [1, 0]
+#if L_or_R == 0:
+#    ball_vel = [random.uniform(-1, -0.5), random.uniform(-1, 1)]
+#else:
+#   ball_vel = [random.uniform(0.5, 1), random.uniform(-1, 1)]
 
 
 pygame.init()
@@ -30,36 +32,43 @@ pygame.display.set_caption("siusiak")
 def init():
     global paddle1_pos, paddle2_pos, paddle1_vel, paddle2_vel, ball_pos
     global score1, score2
-    paddle1_pos = H_HEIGHT - 32
-    paddle2_pos = H_HEIGHT - 32
+    paddle1_pos = [32, H_HEIGHT - 32]
+    paddle2_pos = [WIDTH - 48, H_HEIGHT - 32]
     ball_pos = [H_WIDTH, H_HEIGHT]
 
 
 def draw(window):
-    global paddle1_pos, paddle2_pos, ball_pos
+    global paddle1_pos, paddle2_pos, ball_pos, ball_vel
     window.fill(WHITE)
     pygame.draw.rect(window, BLACK, (0, 8, WIDTH, HEIGHT - 16))
     pygame.draw.line(window, WHITE, (H_WIDTH, 0), (H_WIDTH, HEIGHT))
-    pygame.draw.rect(window, WHITE, (32, paddle1_pos, 16, 64))
-    pygame.draw.rect(window, WHITE, (WIDTH - 48, paddle2_pos, 16, 64))
+    pygame.draw.rect(window, WHITE, (paddle1_pos[0], paddle1_pos[1], 16, 64))
+    pygame.draw.rect(window, WHITE, (paddle2_pos[0], paddle2_pos[1], 16, 64))
     pygame.draw.circle(window, WHITE, (int(ball_pos[0]), int(ball_pos[1])), 10, 0)
 
-    if paddle1_pos > 8 and paddle1_pos < (HEIGHT - 72):
-        paddle1_pos += paddle1_vel
-    elif paddle1_pos == 8 and paddle1_vel > 0:
-        paddle1_pos += paddle1_vel
-    elif paddle1_pos == (HEIGHT - 72) and paddle1_vel < 0:
-        paddle1_pos += paddle1_vel
-    if paddle2_pos > 8 and paddle2_pos < (HEIGHT - 72):
-        paddle2_pos += paddle2_vel
-    elif paddle2_pos == 8 and paddle2_vel > 0:
-        paddle2_pos += paddle2_vel
-    elif paddle2_pos == (HEIGHT - 72) and paddle2_vel < 0:
-        paddle2_pos += paddle2_vel
-
+    if paddle1_pos[1] > 8 and paddle1_pos[1] < (HEIGHT - 72):
+        paddle1_pos[1] += paddle1_vel
+    elif paddle1_pos[1] == 8 and paddle1_vel > 0:
+        paddle1_pos[1] += paddle1_vel
+    elif paddle1_pos[1] == (HEIGHT - 72) and paddle1_vel < 0:
+        paddle1_pos[1] += paddle1_vel
+    if paddle2_pos[1] > 8 and paddle2_pos[1] < (HEIGHT - 72):
+        paddle2_pos[1] += paddle2_vel
+    elif paddle2_pos[1] == 8 and paddle2_vel > 0:
+        paddle2_pos[1] += paddle2_vel
+    elif paddle2_pos[1] == (HEIGHT - 72) and paddle2_vel < 0:
+        paddle2_pos[1] += paddle2_vel
 
     ball_pos[0] += ball_vel[0]
     ball_pos[1] += ball_vel[1]
+
+    if ball_pos[1] < 19 or ball_pos[1] > (HEIGHT - 18):
+        ball_vel[1] = -ball_vel[1]
+
+    elif ball_pos[0] == 48 and ball_pos[1] >= paddle1_pos[1] and ball_pos[1] <= (paddle1_pos[1] + 64):
+        ball_vel[0] = -ball_vel[0]
+    elif ball_pos[0] == 752 and ball_pos[1] >= paddle2_pos[1] and ball_pos[1] <= (paddle2_pos[1] + 64):
+        ball_vel[0] = -ball_vel[0]
 
     text = pygame.font.SysFont("", 64)
     score = text.render( (str(score1) + " : " + str(score2)), 1, WHITE)
